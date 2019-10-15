9cb9012fc4bf330f4d3137d8b9628e73671fda7b
epndavis <elliot.davis@gmail.com>
2019-10-15 19:32:54 +0100

    Add register view and vue

--- app/Http/Controllers/Api/NewsletterController.php
+++ app/Http/Controllers/Api/NewsletterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\StoreNewsletter;
+use App\Models\Email;
 
 class NewsletterController
 {
--- app/Http/Requests/StoreNewsletter.php
+++ app/Http/Requests/StoreNewsletter.php
@@ -13,7 +13,7 @@ class StoreNewsletter extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class StoreNewsletter extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|unique:email',
+            'email' => 'required|unique:emails',
             'first_name' => 'required|string',
             'last_name' => 'required|string',
         ];
